468a103352270b2fec50c7833eed2136dd459aba
Victor SURZUR <victor.surzur@gmail.com>
2025-08-08 19:37:10 +0200

    Setup main scene, autoloads, and input map

--- ui/TitleScreen.gd
+++ ui/TitleScreen.gd
@@ -1,9 +1,18 @@
-﻿extends Control
+extends Control
+
+@onready var btn_new: Button = $VBox/BtnNew
+@onready var btn_quit: Button = $VBox/BtnQuit
 
 func _ready() -> void:
-    print("Title screen ready")
+	btn_new.pressed.connect(_start_game)
+	btn_quit.pressed.connect(func(): get_tree().quit())
 
-func _input(event):
-    if event.is_action_pressed("ui_accept"):
-        print("Start game")
-        # Ici, on changera de scène plus tard
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		_start_game()
+
+func _start_game() -> void:
+	if ResourceLoader.exists("res://local3d/Colony.tscn"):
+		get_tree().change_scene_to_file("res://local3d/Colony.tscn")
+	else:
+		push_error("Colony.tscn manquant (res://local3d/Colony.tscn)")
