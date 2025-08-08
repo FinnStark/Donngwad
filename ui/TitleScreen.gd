60fb227e7747add72c6f7dc7a812fac440d9022b
Victor SURZUR <victor.surzur@gmail.com>
2025-08-08 19:27:48 +0200

    Setup main scene, autoloads, and input map

---
+++ ui/TitleScreen.gd
@@ -0,0 +1,9 @@
+﻿extends Control
+
+func _ready() -> void:
+    print("Title screen ready")
+
+func _input(event):
+    if event.is_action_pressed("ui_accept"):
+        print("Start game")
+        # Ici, on changera de scène plus tard
